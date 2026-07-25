ebd576ae9d5e52c77ce68dcd9d1d2d1eec65663a
João <joaovitorresende.jr@gmail.com>
2026-07-25 15:33:53 -0300

    chore: criando svg dinamico e mais configuracoes de bibliotecas externas svgr

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import Text from "./components/text";
-
+import TrashIcon from "./assets/icons/Trash.svg?react";
 export default function App() {
 
   return (
-    <Text variant={"body-md"}>ola mundo</Text>
-    )
-  
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-1">
+        <Text variant={"body-md"}>ola mundo</Text>
+      </div>
+      <div className="flex gap-1">
+        <TrashIcon className="fill-green-base"/>
+      </div>
+    </div>
+  )
+
 }
