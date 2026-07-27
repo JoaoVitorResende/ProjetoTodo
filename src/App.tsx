5482415923a1f4adddeb8103a271b5437885d4f7
João <joaovitorresende.jr@gmail.com>
2026-07-27 18:49:20 -0300

    chore: novo componente button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Text from "./components/text";
 import TrashIcon from "./assets/icons/Trash.svg?react";
 import SpinIcon from "./assets/icons/spinner.svg?react";
+import PlusIcon from "./assets/icons/Plus-Regular.svg?react";
 import Icon from "./components/icon";
 import Badge from "./components/badge";
+import Button from "./components/button";
 export default function App() {
 
   return (
@@ -17,6 +19,9 @@ export default function App() {
       <div>
         <Badge variant={"secondary"}>5</Badge>
         <Badge variant={"primary"}>2 de 5</Badge>
+      </div>
+      <div>
+        <Button icon={PlusIcon}>Nova task</Button>
       </div>
     </div>
   )
