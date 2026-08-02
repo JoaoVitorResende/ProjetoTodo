032122b3c06dcfea6ab8ebd0dbccab5fb0bda908
João <joaovitorresende.jr@gmail.com>
2026-08-02 12:54:50 -0300

    feat: novos componentes de card e container para centralizacao da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -8,36 +8,43 @@ import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
 import InputText from "./components/Input-text";
 import InputCheckBox from "./components/input-checkbox";
+import Card from "./components/card";
+import Container from "./components/container";
 export default function App() {
 
   return (
-    <div className="grid gap-3">
-      <div className="flex flex-col gap-1">
-        <Text variant={"body-md"}>ola mundo</Text>
+    <Container>
+      <div className="grid gap-3">
+        <div className="flex flex-col gap-1">
+          <Text variant={"body-md"}>ola mundo</Text>
+        </div>
+        <div className="flex gap-1">
+          <Icon svg={TrashIcon} className="fill-green-base" />
+          <Icon svg={SpinIcon} animate />
+        </div>
+        <div>
+          <Badge variant={"secondary"}>5</Badge>
+          <Badge variant={"primary"}>2 de 5</Badge>
+        </div>
+        <div>
+          <Button icon={PlusIcon}>Nova task</Button>
+        </div>
+        <div className="flex gap-1">
+          <ButtonIcon icon={TrashIcon} />
+          <ButtonIcon icon={TrashIcon} variant={"secondary"} />
+          <ButtonIcon icon={TrashIcon} variant={"tertiary"} />
+        </div>
+        <div>
+          <InputText />
+        </div>
+        <div>
+          <InputCheckBox />
+        </div>
+        <div>
+          <Card size="md">teste</Card>
+        </div>
       </div>
-      <div className="flex gap-1">
-        <Icon svg ={TrashIcon} className="fill-green-base"/>
-        <Icon svg ={SpinIcon} animate/>
-      </div>
-      <div>
-        <Badge variant={"secondary"}>5</Badge>
-        <Badge variant={"primary"}>2 de 5</Badge>
-      </div>
-      <div>
-        <Button icon={PlusIcon}>Nova task</Button>
-      </div>
-      <div className="flex gap-1">
-        <ButtonIcon icon={TrashIcon}/>
-        <ButtonIcon icon={TrashIcon} variant={"secondary"}/>
-        <ButtonIcon icon={TrashIcon} variant={"tertiary"}/>
-      </div>
-      <div>
-        <InputText/>
-      </div>
-      <div>
-        <InputCheckBox/>
-      </div>
-    </div>
+    </Container>
   )
 
 }
---
+++ src/components/container.tsx
@@ -0,0 +1,32 @@
+import { type VariantProps, cva } from "class-variance-authority";
+import React from "react";
+import type { Container } from "react-dom/client";
+
+export const containerVariants = cva("mx-auto",{
+    variants:{
+        size:{
+            md: "max-w-126 px-2"
+        }
+    },
+    defaultVariants:{
+        size: "md"
+    }
+});
+
+interface ContainerProps extends VariantProps<typeof containerVariants>, React.ComponentProps<"div">{
+    as?: keyof React.JSX.IntrinsicElements;
+}
+
+export default function Container({
+    as = "div",
+    children,
+    className,
+    ...props
+}: ContainerProps){
+    return React.createElement(as,{
+        className: containerVariants({size: "md", className}),
+        ...props
+    },
+    children
+    )
+}
