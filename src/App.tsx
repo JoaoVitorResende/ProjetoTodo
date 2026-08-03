1bae5739ee7f5974deff167242bcb9b6722b816f
João <joaovitorresende.jr@gmail.com>
2026-08-03 20:33:53 -0300

    feat: componente skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import InputText from "./components/Input-text";
 import InputCheckBox from "./components/input-checkbox";
 import Card from "./components/card";
 import Container from "./components/container";
+import Skeleton from "./components/skeleton";
 export default function App() {
 
   return (
@@ -42,6 +43,10 @@ export default function App() {
         </div>
         <div>
           <Card size="md">teste</Card>
+        </div>
+        <div className="space-y-2">
+          <Skeleton className="h-6"/>
+          <Skeleton className="w-96 h-6"/>
         </div>
       </div>
     </Container>
